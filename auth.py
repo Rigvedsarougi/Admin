9d802b7a89ef602d51d4497a402a5184fa7def90
Rigved Sarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2025-01-31 22:37:14 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,14 @@
+# auth.py
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import User, UserRole
+import bcrypt
+
+def verify_password(plain_password, hashed_password):
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user or not verify_password(password, user.password):
+        return None
+    return user
